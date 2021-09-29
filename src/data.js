485d0d545662fdb6b6e689e38ecd47e254c235c1
B.Suresh <bskumawat3132@gmail.com>
2021-09-29 23:41:28 +0530

    Added Slider component

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        title: "SUMMER SALE",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore cum exercitationem dolores.",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        title: "WINTER SALE",
+        desc: "Winter lorem dolor sit amet consectetur, adipisicing elit. Dolore cum exercitationem dolores.",
+        bg: "f5fafd"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        title: "RAINY SALE",
+        desc: "Rainy ipsum lorem sit amet consectetur, adipisicing elit. Dolore cum exercitationem dolores.",
+        bg: "f5fafd"
+    }
+]
